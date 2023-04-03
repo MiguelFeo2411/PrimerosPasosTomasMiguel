d5157f2378dbbb2a517b42f24cbd6cda3efce988
Miguel Feo <mfemerchan@gmail.com>
2023-04-03 18:35:14 -0500

    Crear la lista definida de cursos con id

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-
 function CourseList() {
   const[cursos, setCursos] =useState([
   {
     id:5100,
-    nombre: "Calculo Integral"
+    nombre: "Arte y geometria"
   },
   {
     id:5101,
@@ -20,26 +17,4 @@ function CourseList() {
     nombre: 'Logica Digital'
   }
   ]);
-  const [nuevoCurso, setNuevoCurso]=useState(" ");
-  const [idNuevo, setNuevoId]=useState(" ");
-  const cursoNuevo = {
-    id: Math.round(Math.random()*10000),
-    texto: nuevoCurso,
-  };
-  setCursos([...cursos, cursoNuevo]);
-  setIdNuevo([...cursos, idNuevo])
-  setNuevoCurso("");
-  setNuevoId("");
-
-
-
-  return(
-      <div> 
-      <h1>Lista de cursos</h1>
-        <input type='text' value={nuevoCurso}/>
-        <input type='text' value={idNuevo}/>
-    </div>
-    );
-};
-
-export default CourseList;
+}
